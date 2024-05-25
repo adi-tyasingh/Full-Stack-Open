d330253279337cf8d7699faf5a76ea4c83ee8d5e
aditya Singh <singhap_2@rknec.edu>
2024-05-25 16:35:04 +0530

    Practice application

--- part2/practice-app/src/App.jsx
+++ part2/practice-app/src/App.jsx
@@ -1,18 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Note from './components/Note'
 
-const App = (props) => {
-  const { notes } = props
+const App = ({ notes }) => {
+
+  const [note, setNotes] = useState(notes)
+  const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    //console.log("button clicked", event.target)
+    const noteObject={
+      id: note.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    setNotes(note.concat(noteObject))
+    setNewNote("")
+  }
+
+  const handleNoteChange = (event) => {
+    //console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? note : note.filter(x => x.important)
+
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {
-          notes.map(note => <li key={note.id}> {note.content} </li>)
-        }
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
+      <button onClick={()=> setShowAll(!showAll)}> 
+          show {showAll? "important":"all"}
+      </button>
     </div>
   )
 }
