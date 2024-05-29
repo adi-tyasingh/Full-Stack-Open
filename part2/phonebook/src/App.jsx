4f6ede3f8fb8cc1df775ba70577269eb71988aef
aditya Singh <singhap_2@rknec.edu>
2024-05-30 00:51:51 +0530

    phonebook steps 7 - 10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 import Contacts from './components/Contacts'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
@@ -11,27 +11,56 @@ const App = () => {
   const [searchContact, setSearchContact] = useState('')
 
   const hook = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then( (response) => setPersons(response.data))
+    personService
+      .getAll()
+      .then(response => setPersons(response))
   }
   
   useEffect(hook,[])
 
   const addNumber = (event) => {
     event.preventDefault()
-    if (persons.some((element) => element.name.toLowerCase() === newName.toLowerCase())){
-      alert(`${newName} is already present`)
-      return
+
+    if (persons.some((element) => element.name.trim().toLowerCase() === newName.trim().toLowerCase())){
+      if ( window.confirm( `${newName} already exists! would you like to change the number?` ) )
+      {
+        const person = persons.find((element) => element.name.trim().toLowerCase() === newName.trim().toLowerCase())
+        const newContact = {
+          ...person, 
+          number: newNumber
+        }
+        personService
+          .update(person.id, newContact)
+          .then(response => {
+            setPersons( persons.map( p => p.id === person.id ? newContact : p ) )
+            setNewName('')
+            setNewNumber('')
+          })
+        return
+      }
     }
+
     const newContact = {
       name: newName,
       number : newNumber
     }
-    setPersons(persons.concat(newContact))
-    setNewName('')
-    setNewNumber('')
-  } 
+    personService
+      .create(newContact)
+      .then( response => {
+        setPersons( persons.concat(response) )
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+  
+  const destroyContact = (id) => {
+    const person = persons.find(p => p.id === id)
+    if(window.confirm(`Do you want to delete ${person.name} ?`)){
+      personService
+        .destroy(id)
+        .then(response => setPersons( persons.filter(person => person.id !== id) ))
+    }
+  }
   
   const toShow = persons.filter(person => person.name.toLowerCase().includes(searchContact.toLowerCase()))
 
@@ -43,7 +72,7 @@ const App = () => {
       addNumber={addNumber} 
       newName={newName} setNewName={setNewName} 
       newNumber={newNumber} setNewNumber={setNewNumber}/>
-      <Contacts contacts={toShow}/>
+      <Contacts contacts={toShow} destroyContact={destroyContact}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Contacts.jsx
+++ part2/phonebook/src/components/Contacts.jsx
@@ -1,14 +1,14 @@
-const Contacts = ({contacts}) => {
+const Contacts = ({contacts, destroyContact}) => {
     return(
       <div>
         <h2>Numbers</h2>
         <ul>
-          {contacts.map(contact =><Contact key={contact.name} contact={contact}/>)}
+          {contacts.map(contact =><Contact key={contact.id} contact={contact} destroyContact={() => destroyContact(contact.id)}/>)}
         </ul>
       </div>
     )
 }
 
-const Contact = ({contact}) => <li>{contact.name} : {contact.number}</li>
+const Contact = ({contact, destroyContact}) => <li>{contact.name} : {contact.number} <button onClick={destroyContact}> Delete</button></li>
 
 export default Contacts
